e2f451cd2beec4ab20aea54a18ffc62c00f76d2c
baluworks <baluworks@gmail.com>
2019-10-06 16:16:36 +0530

    Initial commit

---
+++ index.ts
@@ -0,0 +1,34 @@
+// Import stylesheets
+import './style.css';
+
+//Enumerations :
+enum Color {RED,GREEN,BLUE};
+
+
+let colorValue :Color = Color.BLUE;
+let colorString:String = Color[colorValue];
+let data:String ="";
+data+=`<br>${colorString}-${colorValue}`;
+
+//Enumetarions
+enum Kid {Yoshita,Mukunda,Nandana,Anjana,yuvan,Bhuvi};
+
+Object.keys(Kid).map( val=>{
+  console.log(val);
+});
+
+let firstKid:String =Kid[0];
+let lastKid:String =Kid[(Object.keys(Kid).length/2 -1)];
+data+=`<br>${firstKid}-${lastKid}`;
+
+let tuple:[string,number,boolean];
+tuple =['Hello', 0,true];
+data+=`<br>Tuple Example : ${tuple[0]}-${tuple[1]}-${tuple[2]}`;
+
+console.log(tuple)
+
+let anyObject:any= {}
+console.log(anyObject);
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter ${data}</h1>`;
